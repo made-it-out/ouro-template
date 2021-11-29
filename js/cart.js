29f0c1e5229c36d6aaacfce12330e8afd7704592
made-it-out <mike.hallisy@gmail.com>
2021-11-29 18:56:05 -0500

    updated/added cart page

--- js/cart.js
+++ js/cart.js
@@ -1,40 +1,80 @@
-// Product Quantity
-let subtotal = document.querySelector('.cart__subtotal-number')
-let products = document.querySelectorAll('.cart__product');
+document.addEventListener('DOMContentLoaded', init)
 
-if (products) {
-    products.forEach(product => {
-        let qtyMinus = product.querySelector('.cart__product-quantity-minus');
-        let qtyPlus = product.querySelector('.cart__product-quantity-plus');
-        let qtyAmount = product.querySelector('.cart__product-quantity-picker');
-        let productPrice = product.querySelector('.cart__product-price-number');
-        let productTotal = product.querySelector('.cart__product-total-number');
-        let removeBtn = product.querySelector('.cart__product-remove-btn');
+function init() {
+    let subtotal = document.querySelector('.cart__subtotal-amount')
+    let products = Array.from(document.querySelectorAll('.cart__product'));
 
-        // Only allow number inputs to the quantity
-        qtyAmount.addEventListener('input', (e) => {
-            if (isNaN(e.target.value)) {
-                e.target.value = '';
+    if (products) {
+        products.forEach(product => {
+            const qtyMinus = product.querySelector('.cart__product-quantity-minus');
+            const qtyPlus = product.querySelector('.cart__product-quantity-plus');
+            let qtyInput = product.querySelector('.cart__product-quantity-input');
+            let price = product.querySelector('.cart__product-price');
+            let priceNumber = price.textContent.slice(1);
+            let total = product.querySelector('.cart__product-price-total');
+            let removeBtn = product.querySelector('.cart__product-remove-btn');
+
+
+            // Quantity
+            qtyPlus.addEventListener('click', () => {
+                qtyIncrement();
+                updateSubtotal();
+            })
+            qtyPlus.addEventListener('keydown', (event) => {
+                if (event.keyCode === 13 || event.keyCode === 32) {
+                    qtyIncrement();
+                    updateSubtotal();
+                }
+            })
+            qtyMinus.addEventListener('click', () => {
+                qtyDecrement();
+                updateSubtotal();
+            });
+            qtyMinus.addEventListener('keydown', (event) => {
+                if (event.keyCode === 13 || event.keyCode === 32) {
+                    qtyDecrement();
+                    updateSubtotal();
+                }
+            })
+
+            function qtyIncrement() {
+                qtyInput.value++;
+                total.textContent = `$${(qtyInput.value * priceNumber).toFixed(2)}`
+            }
+            function qtyDecrement() {
+                if (qtyInput.value > 1) {
+                    qtyInput.value--;
+                    total.textContent = `$${(qtyInput.value * priceNumber).toFixed(2)}`
+                }
+            }
+
+            // Remove Button
+            removeBtn.addEventListener('click', () => {
+                removeProduct(product);
+                updateSubtotal();
+            });
+            removeBtn.addEventListener('keydown', (event) => {
+                if (event.keyCode === 13 || event.keyCode === 32) {
+                    removeProduct(product);
+                    updateSubtotal();
+                }
+            })
+
+            function removeProduct(product) {
+                product.remove();
+                products = products.filter(prod => prod !== product);
             }
         })
-        // Decrement
-        qtyMinus.addEventListener('click', () => {
-            if (qtyAmount.value > 1) {
-                qtyAmount.value = qtyAmount.value - 1;
-                productTotal.textContent = (qtyAmount.value * parseFloat(productPrice.textContent)).toFixed(2);
-                subtotal.textContent = (parseFloat(subtotal.textContent) - parseFloat(productPrice.textContent)).toFixed(2);
-            }
-        })
-        // Increment
-        qtyPlus.addEventListener('click', () => {
-            qtyAmount.value++;
-            productTotal.textContent = (qtyAmount.value * parseFloat(productPrice.textContent)).toFixed(2);
-            subtotal.textContent = (parseFloat(subtotal.textContent) + parseFloat(productPrice.textContent)).toFixed(2);
-        })
-        // Remove
-        removeBtn.addEventListener('click', () => {
-            subtotal.textContent = (parseFloat(subtotal.textContent) - parseFloat(productTotal.textContent)).toFixed(2);
-            product.remove();
-        })
-    })
+    }
+    function updateSubtotal() {
+        let totals = Array.from(document.querySelectorAll('.cart__product-price-total'));
+
+        let totalsNumbers = totals.map(total => total.textContent.slice(1));
+
+        if (products && products.length > 0) {
+            subtotal.textContent = `$${totalsNumbers.reduce((previous, current) => (parseFloat(previous) + parseFloat(current)).toFixed(2))}`;
+        } else {
+            subtotal.textContent = '$0.00'
+        }
+    }
 }
